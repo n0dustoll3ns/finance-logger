b85a5ca3115b077d501a7c676a9fdff07280ccfb
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-16 23:53:03 +0700

    add classes to strcture logic

--- public/app.js
+++ public/app.js
@@ -1,13 +1,22 @@
 "use strict";
-const anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
 }
-console.log(anchor.href);
-//const form = document.querySelector('form')!;
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
-// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
